80775b59a9ecfd79734d9f62d7fcae9f5cf0ef71
Matheus Pestana <132798705+math1p@users.noreply.github.com>
2024-03-22 23:46:47 -0300

    Update config.h

--- ESP-Dallas/config.h
+++ ESP-Dallas/config.h
@@ -1,10 +1,10 @@
-#define BLYNK_TEMPLATE_ID "TMPL2qIWkHS-J"
-#define BLYNK_TEMPLATE_NAME "Temperature"
-#define BLYNK_AUTH_TOKEN "dx5rkZB1TvJiPMKQqVPPQVn4JDs4jga8"
+#define BLYNK_TEMPLATE_ID "  "
+#define BLYNK_TEMPLATE_NAME "  "
+#define BLYNK_AUTH_TOKEN "  "
 
-char auth[] = "dx5rkZB1TvJiPMKQqVPPQVn4JDs4jga8";
-char ssid[] = "5406 2.4GHz";
-char pass[] = "25112911";
+char auth[] = "  ";
+char ssid[] = "  ";
+char pass[] = "  ";
 
 #define sendDataDelay 30L * 60L * 1000L  // Set Blynk and DHT22 delay (delay > 2000ms)
 
