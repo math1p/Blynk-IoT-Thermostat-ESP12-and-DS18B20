87be8c03ae07fafa7d37926a755fe948d75e9909
Matheus Pestana <132798705+math1p@users.noreply.github.com>
2024-03-24 22:57:51 -0300

    Create config.h

---
+++ ESP-Dallas/config.h
@@ -0,0 +1,11 @@
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+
+#define sendDataDelay 30L * 60L * 1000L  // Set Blynk and Dallas delay (delay > 2000ms) (1L * 30L * 1000L - 30s)
+
+#define ONE_WIRE_BUS 0  // Dallas
