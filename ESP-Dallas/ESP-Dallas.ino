8e55143872293af470be4590365ef70cf353db2c
Matheus Pestana <132798705+math1p@users.noreply.github.com>
2024-03-24 22:56:28 -0300

    Update ESP-Dallas.ino

--- ESP-Dallas/ESP-Dallas.ino
+++ ESP-Dallas/ESP-Dallas.ino
@@ -3,21 +3,25 @@
 #include <BlynkSimpleEsp8266.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#define GPIO2 2
+#define BLYNK_PRINT Serial
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 BlynkTimer timer;
 
-float temperatureC = 0;
-
-void sendData() {
-  updateData();
-  Blynk.virtualWrite(V0, temperatureC);
-}
+float temperatureC;
 
 void setup() {
+  pinMode(GPIO2, OUTPUT);
+  digitalWrite(GPIO2, LOW);
   delay(1000);
+  digitalWrite(GPIO2, HIGH);
+  delay(1000);
+  digitalWrite(GPIO2, LOW);
+  delay(1000);
+  digitalWrite(GPIO2, HIGH);
   Blynk.begin(auth, ssid, pass);
   timer.setInterval(sendDataDelay, sendData);
 }
@@ -27,7 +31,16 @@ void loop() {
   timer.run();
 }
 
+void sendData() {
+  updateData();
+  Blynk.virtualWrite(V0, temperatureC);
+  // wifi_set_sleep_type(LIGHT_SLEEP_T)
+}
+
 void updateData() {
-  sensors.requestTemperatures();  // Solicitar leitura da temperatura
+  digitalWrite(GPIO2, LOW);
+  delay(2000);
+  digitalWrite(GPIO2, HIGH);
+  sensors.requestTemperatures();              // Solicitar leitura da temperatura
   temperatureC = sensors.getTempCByIndex(0);  // Obter a temperatura em graus Celsius
 }
